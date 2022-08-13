abf3aa9b3b898326cf55805ac06cfb026166ac36
UnixBear <unixbear@gmail.com>
2022-08-12 23:46:43 -0700

    updated deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,9 @@
-mechaCarMpg <- read.csv("MechaCar_mpg.csv")
-library(dplyr)
-mechaLinear <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechaCarMpg)
+mechaCarMpg <- read.csv("MechaCar_mpg.csv", stringsAsFactors = F) #Loading mpg dataset
+library(dplyr) #imporing dplyr library
+mechaLinear <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mechaCarMpg) #creating linear model
 mechaPvals <- temp$coefficients[,4]
+summary(mechaLinear)
+
+suspensionCoil <- read.csv("Suspension_Coil.csv", stringsAsFactors = F) #Loading suspension coil dataset
+total_summary <- suspensionCoil %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI), StdDev=sd(PSI)) #Create basic summary dataframe
+lot_summary <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI),Variance=var(PSI), StdDev=sd(PSI)) #create summary dataframe per lot
