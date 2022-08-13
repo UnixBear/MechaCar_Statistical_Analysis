720b0f477d1a8b05a8b39b3c752698b82070a6e6
UnixBear <unixbear@gmail.com>
2022-08-13 06:28:35 -0400

    small change to include testing in the r file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,6 @@ t.test(suspensionCoil$PSI, mu=1500) #t test all with a mean of 1500
 lot1 <- subset(suspensionCoil, Manufacturing_Lot == 'Lot1') #creating a set of vehicles from lot 1
 lot2 <- subset(suspensionCoil, Manufacturing_Lot == 'Lot2') #creating a set of vehicles from lot 2
 lot3 <- subset(suspensionCoil, Manufacturing_Lot == 'Lot3') #creating a set of vehicles from lot 3
+t.test(lot1$PSI, mu=1500)
+t.test(lot2$PSI, mu=1500)
+t.test(lot3$PSI, mu=1500) # t tests on all three lots
